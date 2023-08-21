ff870ac937e352b9a2a653cddd10d88850693d10
anas <anasseel84@gmail.com>
2023-08-21 16:34:28 -0700

    main_0.c

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -1,19 +1,11 @@
 #include "main.h"
 
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
 int main(void)
 {
     int r;
+    int n = 'a'; // Replace 'a' with the character you want to test
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
+    r = _islower(n);
     _putchar(r + '0');
     _putchar('\n');
     return (0);
